cb8d0950dc0e8134c2bf326597cb723fab2a0ff3
xiasummer740 <xiasummer740@users.noreply.github.com>
2026-08-02 14:23:23 +0800

    fix: timed_task模型补Text导入

--- backend/models/timed_task.py
+++ backend/models/timed_task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
 from sqlalchemy.sql import func
 from database import Base
 
